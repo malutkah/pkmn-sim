82767c5f4c1e5cee319163413dc6ac9dd886c56f
malutkah <73688226+malutkah@users.noreply.github.com>
2022-04-12 11:03:02 +0200

    added type testing scene

--- Pokemon_Sim/Assets/_Scripts/Calculation/TypeCalculation.cs
+++ Pokemon_Sim/Assets/_Scripts/Calculation/TypeCalculation.cs
@@ -1,8 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public static class TypeCalculation
+public class TypeCalculation : MonoBehaviour
 {
-    
+    public TMP_Dropdown offenseDrop;
+    public TMP_Dropdown defenseDrop1;
+    public TMP_Dropdown defenseDrop2;
+    public TMP_Text valueText;
+
+    private float effectValueMultiplicator;
+    private string defenseType1;
+    private string defenseType2;
+    private string offenseType;
+
+    private void Start()
+    {
+        CalculateType();
+    }
+
+    public void CalculateType()
+    {
+        offenseType = offenseDrop.options[offenseDrop.value].text;
+        defenseType1 = defenseDrop1.options[defenseDrop1.value].text;
+        defenseType2 = defenseDrop2.options[defenseDrop2.value].text;
+
+        float type1 = Calculations.GetEffectivness(defenseType1, offenseType);
+        float type2 = 1f;
+
+        if (defenseType2 != "None")
+        {
+            type2 = Calculations.GetEffectivness(defenseType2, offenseType);
+        }
+
+        effectValueMultiplicator = type1 * type2;
+
+        string tmpText = effectValueMultiplicator == 2 ? "Super Effective" : effectValueMultiplicator == 0.5f ? "Not Effective" : effectValueMultiplicator == 1 ? "Normal" : "Nothing";
+
+        valueText.text = tmpText; // effectValueMultiplicator.ToString();
+    }
 }
